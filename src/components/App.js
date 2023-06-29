6d4a2a83f35278b8ead6fc7216d189c93e57c142
Juan Barajas <juan.barajas4406@my.riohondo.edu>
2023-06-29 15:18:47 -0700

    fixes recipe buttons

--- src/components/App.js
+++ src/components/App.js
@@ -2,26 +2,34 @@ import React, { useEffect, useState } from "react"
 import { Route, Switch } from "react-router-dom";
 import AddRecipeForm from "./AddRecipeForm";
 import Album from "./Album";
-
+import RecipeDetails from "./RecipeDetails";
 
 const API = "http://localhost:3001/recipes"
 
 
 function App() {
     const [recipes, setRecipes] = useState([]);
+    const [recipeDetails, setRecipeDetails] = useState({});
 
     useEffect(() => fetch(API)
         .then(res => res.json())
         .then(setRecipes), [])
 
+    function handleRecipeDetailClick(recipe){
+        setRecipeDetails(recipe);
+    }
+
     return (
         <div>
             <Switch>
                 <Route exact path="/">
-                    <Album recipes={recipes} addLike={addLike} removeLike={removeLike}/>
+                    <Album recipes={recipes} addLike={addLike} removeLike={removeLike} recipeDetailClick={handleRecipeDetailClick}/>
                 </Route>
                 <Route path="/new" >
                     <AddRecipeForm />
+                </Route>
+                <Route path="/recipe-details">
+                    <RecipeDetails recipe={recipeDetails}/>
                 </Route>
             </Switch>
         </div>
